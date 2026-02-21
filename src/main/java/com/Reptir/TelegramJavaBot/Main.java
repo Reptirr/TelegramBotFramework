f8d5c5e91bdc39827784d710b3b42a962a76b312
Reptir <forgetwo5@gmail.com>
2026-02-21 16:49:15 +0700

    bot token revoked

--- src/main/java/com/Reptir/TelegramJavaBot/Main.java
+++ src/main/java/com/Reptir/TelegramJavaBot/Main.java
@@ -5,13 +5,16 @@ import com.Reptir.TelegramJavaBot.Framework.Core.Telegram.BotBuilder;
 import com.Reptir.TelegramJavaBot.Realizations.CommandsRealization.ActionCommand;
 import com.Reptir.TelegramJavaBot.Realizations.CommandsRealization.StartCommand;
 
-import java.util.Map;
-import java.util.concurrent.Future;
+/*
+* TODO:
+*  1. Система диалогов:
+*  - Регистр <String userId, UserState>
+*  - UserState - это интерфейс с 3 полями: String currentAction, long timestamp (seconds), BaseCommand ExecutionCommand
+*/
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        // bot by me: @TestBotByReptir_bot (its token in there)
-        Bot bot = BotBuilder.builder("8537210836:AAHjOov883WvvYYFutXtzBzea0BTDw8qUHc") // your token bot. you can get it be @BotFather in telegram.
+        Bot bot = BotBuilder.builder("YOUR_TOKEN") // your token bot. you can get it be @BotFather in telegram.
                 .command(new ActionCommand()) // you must register your command there
                 .command(new StartCommand())
                 .build();
