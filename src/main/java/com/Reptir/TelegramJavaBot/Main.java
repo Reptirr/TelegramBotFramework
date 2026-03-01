d5ef39a4cbb2c58dd326b379e35c3376b41b9dc6
Reptir <forgetwo5@gmail.com>
2026-03-01 12:53:30 +0700

    refactor: remove unused imports

--- src/main/java/com/Reptir/TelegramJavaBot/Main.java
+++ src/main/java/com/Reptir/TelegramJavaBot/Main.java
@@ -1,13 +1,11 @@
 package com.Reptir.TelegramJavaBot;
 
-import com.Reptir.TelegramJavaBot.Framework.Core.Registries.BotUser;
 import com.Reptir.TelegramJavaBot.Framework.Core.Telegram.Bot;
 import com.Reptir.TelegramJavaBot.Framework.Core.Telegram.BotBuilder;
 import com.Reptir.TelegramJavaBot.Realizations.CommandsRealization.ActionCommand;
 import com.Reptir.TelegramJavaBot.Realizations.CommandsRealization.DialogCommand;
 import com.Reptir.TelegramJavaBot.Realizations.CommandsRealization.StartCommand;
 
-import java.util.Map;
 
 /*
 * TODO:
@@ -16,7 +14,7 @@ import java.util.Map;
 */
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Bot bot = BotBuilder.builder("TOKEN") // your token bot. you can get it in @BotFather in telegram.
                 .command(new ActionCommand()) // you must register your command there
                 .command(new StartCommand())
