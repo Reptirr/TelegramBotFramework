a7eaa5d601ef95745e70116420fdb1dad7d9fe98
Reptir <forgetwo5@gmail.com>
2026-05-26 22:03:05 +0700

    refactor Context: use Update instead of Message and CallbackQuery

--- src/main/java/com/Reptir/TelegramJavaBot/Framework/Core/CommandLogic/Context.java
+++ src/main/java/com/Reptir/TelegramJavaBot/Framework/Core/CommandLogic/Context.java
@@ -3,49 +3,32 @@ package com.Reptir.TelegramJavaBot.Framework.Core.CommandLogic;
 import com.Reptir.TelegramJavaBot.Framework.Core.DialogLogic.DialogManager;
 import com.Reptir.TelegramJavaBot.Framework.Core.Registries.RegistryUser;
 import com.Reptir.TelegramJavaBot.Framework.Core.Telegram.Messenger;
-import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
-import org.telegram.telegrambots.meta.api.objects.message.Message;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
-public class Context { // !
-    private final Message message;
+public class Context {
+    private final Update update;
     private final Messenger messenger;
-    private final CallbackQuery callback;
     private final DialogManager dialogManager;
     private final RegistryUser registryUser;
 
-    public Context(Message message, TelegramClient tgClient, CallbackQuery callback, DialogManager dialogManager, RegistryUser registryUser) {
-        this.message = message;
+    public Context(TelegramClient tgClient, Update update, DialogManager dialogManager, RegistryUser registryUser) {
+        this.update = update;
         this.messenger = new Messenger(tgClient);
-        this.callback = callback;
         this.dialogManager = dialogManager;
         this.registryUser = registryUser;
     }
 
-    public Message getMessage() {
-        return message;
-    }
     public Messenger getMessenger() {
         return messenger;
     }
-    public CallbackQuery getCallback() {
-        return callback;
+    public Update getUpdate() {
+        return update;
     }
     public DialogManager getDialogManager() {
         return dialogManager;
     }
     public RegistryUser getRegistryUser() {
         return registryUser;
-    }
-
-    public void edit(String text, InlineKeyboardMarkup markup) {
-        if (message.getFrom() != null && message.getFrom().getIsBot()) throw new RuntimeException("Cant use ctx.edit() in non-bot messages");
-
-        if (markup != null) {
-            messenger.editText(message.getChatId(), message.getMessageId(), text, markup);
-        } else {
-            messenger.editText(message.getChatId(), message.getMessageId(), text);
-        }
     }
 }
