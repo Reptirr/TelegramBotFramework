3e9058199e9523777e5878330f4837a477c8dfb8
Reptir <forgetwo5@gmail.com>
2026-05-28 00:37:08 +0700

    Revert "improve Context"
    This reverts commit 6a6d7e0f544c040475b0b9b967d7633ab27645bc.

--- src/main/java/com/Reptir/TelegramJavaBot/Framework/Core/CommandLogic/Context.java
+++ src/main/java/com/Reptir/TelegramJavaBot/Framework/Core/CommandLogic/Context.java
@@ -5,27 +5,26 @@ import com.Reptir.TelegramJavaBot.Framework.Core.Registries.RegistryUser;
 import com.Reptir.TelegramJavaBot.Framework.Core.Telegram.ChatType;
 import com.Reptir.TelegramJavaBot.Framework.Core.Telegram.Messenger;
 import com.Reptir.TelegramJavaBot.Framework.Core.Telegram.UpdateType;
-import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.api.objects.chat.Chat;
 import org.telegram.telegrambots.meta.api.objects.message.Message;
 
-public record Context(
-        Messenger messenger,
-        Update update,
-        DialogManager dialogManager,
-        RegistryUser registryUser
-) {
-
+public record Context(Messenger messenger, Update update, DialogManager dialogManager, RegistryUser registryUser) {
     public UpdateType updateType() {
-        if (update.hasMessage()) return UpdateType.USER_INPUT;
-        if (update.hasCallbackQuery()) return UpdateType.CALLBACK;
-        if (update.hasEditedMessage()) return UpdateType.MESSAGE_EDITED;
+        if (update.hasMessage()) {
+            return UpdateType.USER_INPUT;
+        }
+        if (update.hasCallbackQuery()) {
+            return UpdateType.CALLBACK;
+        }
+        if (update.hasEditedMessage()) {
+            return UpdateType.MESSAGE_EDITED;
+        }
         return UpdateType.UNKNOWN;
     }
 
-    public Long chatId() {
+    public long chatId() {
         if (update.hasMessage()) {
             return update.getMessage().getChat().getId();
         } else if (update.hasEditedMessage()) {
@@ -33,7 +32,8 @@ public record Context(
         } else if (update.hasCallbackQuery()) {
             return update.getCallbackQuery().getMessage().getChatId();
         }
-        return null;
+
+        throw new IllegalStateException("Update does not contain chat id");
     }
 
     public User user() {
@@ -44,46 +44,36 @@ public record Context(
         } else if (update.hasCallbackQuery()) {
             return update.getCallbackQuery().getFrom();
         }
-        return null;
+
+        throw new IllegalStateException("Update does not contain user");
     }
 
     public Chat chat() {
         if (update.hasMessage()) {
             return update.getMessage().getChat();
         }
+
         if (update.hasEditedMessage()) {
             return update.getEditedMessage().getChat();
         }
+
         if (update.hasCallbackQuery() && update.getCallbackQuery().getMessage() != null) {
             return update.getCallbackQuery().getMessage().getChat();
         }
-        return null;
+
+        throw new IllegalStateException("Update does not contain chat");
     }
 
     public Message message() {
         if (update.hasMessage()) {
             return update.getMessage();
-        }
-
-        if (update.hasEditedMessage()) {
+        } else if (update.hasEditedMessage()) {
             return update.getEditedMessage();
+        } else if (update.hasCallbackQuery()) {
+            return (Message) update.getCallbackQuery().getMessage();
         }
 
-        if (update.hasCallbackQuery()) {
-            var msg = update.getCallbackQuery().getMessage();
-            if (msg instanceof Message message) {
-                return message;
-            }
-        }
-
-        return null;
-    }
-
-    public CallbackQuery callbackQuery() {
-        if (update.hasCallbackQuery()) {
-            return update.getCallbackQuery();
-        }
-        return null;
+        throw new IllegalStateException("Update does not contain message");
     }
 
     public ChatType chatType() {
@@ -92,4 +82,6 @@ public record Context(
 
         return ChatType.map(chat.getType());
     }
+
+
 }
